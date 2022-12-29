af1a42c7c308e7a00db1e9132a0cde37e3120427
9000HAL <gsanchezscs@gmail.com>
2022-12-29 15:27:56 -0800

    do take home day 1 JS

--- myscript.js
+++ myscript.js
@@ -1,3 +1,14 @@
 function myFunction() {
   document.getElementById("demo").innerHTML = "Paragraph changed.";
 }
+
+console.log("Hello world!");
+
+const myObj = {firstname:"John", lastname:"Doe"};
+console.log(myObj);
+
+console.log("GABE SANCHEZ");
+
+console.log("ENCHILADAS SUIZAS");
+
+
