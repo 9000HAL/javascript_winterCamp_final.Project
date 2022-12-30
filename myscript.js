b742bac33593ac8bf53589d13f1da07ad5d742d2
9000HAL <gsanchezscs@gmail.com>
2022-12-30 13:52:32 -0800

    list js variables per syllabus day 2

--- myscript.js
+++ myscript.js
@@ -4,14 +4,45 @@ function myFunction() {
 
 console.log("Hello world!");
 
+
 const myObj = {firstname:"John", lastname:"Doe"};
 console.log(myObj);
 
+
 console.log("GABE SANCHEZ");
+
 
 console.log("ENCHILADAS SUIZAS");
 
+
 const myArr = ["Orange", "Banana", "Mango", "Kiwi"];
 console.log(myArr);
+
+
+const str = 'Supercalifragilisticexpialidocious';
+console.log(`${str} ${str.length}`);
+
+
+console.log("666");
+console.log(typeof 666);
+
+
+console.log("1234567890123456789012345678901234567890n");
+console.log(typeof 1234567890123456789012345678901234567890n);
+
+
+var x = 1;
+
+if (x === 1) {
+  var x = 2;
+
+  console.log(x);
+}
+
+console.log(x);
+
+
+
+
 
 
