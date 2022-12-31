a80a572c975de27fb6d4c4b78015ab49a4ea7c46
9000HAL <gsanchezscs@gmail.com>
2022-12-30 19:09:05 -0800

    run JS variables console exercises

--- myscript.js
+++ myscript.js
@@ -32,14 +32,49 @@ console.log(typeof 1234567890123456789012345678901234567890n);
 
 
 var x = 1;
-
 if (x === 1) {
   var x = 2;
 
   console.log(x);
 }
-
 console.log(x);
+
+
+
+let  y = 11;
+// Here y is 11
+{  
+  let y = 3;
+  // Here y is 3
+}
+// Here y is 11
+document.getElementById("demo").innerHTML = y;
+
+
+
+const number = 42;
+try {
+  number = 99;
+} catch (err) {
+  console.log(err);
+  // expected output: TypeError: invalid assignment to const `number'
+  // (Note: the exact output may be browser-dependent)
+}
+console.log(number);
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 
 
