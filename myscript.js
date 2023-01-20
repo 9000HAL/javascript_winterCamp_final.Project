16ed392912b746ab8d4524328b2173c6ea9fa661
9000HAL <gsanchezscs@gmail.com>
2023-01-19 18:50:15 -0800

    JavaScript exercise w/ChatGPT

--- myscript.js
+++ myscript.js
@@ -1,71 +1,20 @@
-function myFunction() {
-  document.getElementById("demo").innerHTML = "Paragraph changed.";
-}
+var studentName = document.querySelector(".student").innerHTML;
 
-console.log("Hello world!");
+var schoolName = document.querySelector("h3").innerHTML;
 
+var bestGrade = document.querySelector(".grade-2").innerHTML;
 
-const myObj = {firstname:"John", lastname:"Doe"};
-console.log(myObj);
+var newData = {
+  studentName: "John Doe",
+  schoolName: "Code School",
+  bestGrade: "A+"
+};
 
+document.querySelector('.student').innerHTML = newData.studentName;
 
-console.log("GABE SANCHEZ");
+document.querySelector('h3').innerHTML = newData.schoolName;
 
-
-console.log("ENCHILADAS SUIZAS");
-
-
-const myArr = ["Orange", "Banana", "Mango", "Kiwi"];
-console.log(myArr);
-
-
-const str = 'Supercalifragilisticexpialidocious';
-console.log(`${str} ${str.length}`);
-
-
-console.log("666");
-console.log(typeof 666);
-
-
-console.log("1234567890123456789012345678901234567890n");
-console.log(typeof 1234567890123456789012345678901234567890n);
-
-
-var x = 1;
-if (x === 1) {
-  var x = 2;
-
-  console.log(x);
-}
-console.log(x);
-
-
-
-let  y = 11;
-// Here y is 11
-{  
-  let y = 3;
-  // Here y is 3
-}
-// Here y is 11
-document.getElementById("demo").innerHTML = y;
-
-
-
-const number = 42;
-try {
-  number = 99;
-} catch (err) {
-  console.log(err);
-  // expected output: TypeError: invalid assignment to const `number'
-  // (Note: the exact output may be browser-dependent)
-}
-console.log(number);
-
-
-
-
-
+document.querySelector('.grade-2').innerHTML = newData.bestGrade;
 
 
 
