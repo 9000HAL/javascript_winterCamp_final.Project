83003b996a2fb9fe5f8774237952320cba1c7d16
9000HAL <gsanchezscs@gmail.com>
2023-02-01 18:04:37 -0800

    codepen--->VSC--->final proj_8ball

--- myscript.js
+++ myscript.js
@@ -1,20 +1,36 @@
-var studentName = document.querySelector(".student").innerHTML;
+$(document).ready(function(){
+  
+  var magic8Ball = {};
+  magic8Ball.listofanswers = ["It is certain.", "It is decidedly so.", "Without a doubt.", "Yes, definitely.", "You may rely on it.", "As I see it, yes.", "Most likely.", "Outlook good.", "Yes.", "Signs point to yes.", "Reply hazy, try again.", "Ask again later.", "Better not tell you now.", "Cannot predict now.", "Concentrate and ask again.", "Don't count on it.", "My reply is no.", "My sources say no.", "Outlook not so good.", "Very doubtful."];
+ 
+  magic8Ball.getAnswer = function(question)
+  {
+    var randomNumber = Math.random();
+    var randomAnswer = Math.floor(randomNumber * this.listofanswers.length);
+    var answer = this.listofanswers[randomAnswer];
+    
+    $("#8ball").effect( "shake" );
+    $("#answer").text( answer );
+    $("#answer").fadeIn(3000);
+    $("#8ball").attr("src", "https://s3.amazonaws.com/media.skillcrush.com/skillcrush/wp-content/uploads/2016/09/answerside.png");
 
-var schoolName = document.querySelector("h3").innerHTML;
+    console.log(question);
+    console.log(answer);
+  };
+  $("#answer").hide();
 
-var bestGrade = document.querySelector(".grade-2").innerHTML;
+  var onClick = function()
+  {
+    $("#answer").hide();
+    $("#8ball").attr("src", "https://s3.amazonaws.com/media.skillcrush.com/skillcrush/wp-content/uploads/2016/09/magic8ballQuestion.png");
+    var question = prompt("What do you want to know?");
+    magic8Ball.getAnswer(question);
+  };
+  
+  $("#questionButton").click( onClick );
+});
 
-var newData = {
-  studentName: "John Doe",
-  schoolName: "Code School",
-  bestGrade: "A+"
-};
 
-document.querySelector('.student').innerHTML = newData.studentName;
-
-document.querySelector('h3').innerHTML = newData.schoolName;
-
-document.querySelector('.grade-2').innerHTML = newData.bestGrade;
 
 
 
